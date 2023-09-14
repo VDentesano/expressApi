5a09efaaf5892299934a3250f3a4d99cdb9c22ad
valentinodentesano <valentinodentesano@letsbit.io>
2023-09-14 11:25:41 -0300

    Feat(Dev): Add standard and cors dependencies

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 let notes = [
   {
     id: 1,
-    titulo: "Nota 1",
-    contenido: "Esta es la primera nota.",
-    fecha_creacion: "2023-09-12T10:00:00Z",
+    titulo: 'Nota 1',
+    contenido: 'Esta es la primera nota.',
+    fecha_creacion: '2023-09-12T10:00:00Z',
     important: false
   },
   {
     id: 2,
-    titulo: "Nota 2",
-    contenido: "Esta es la segunda nota.",
-    fecha_creacion: "2023-09-12T12:30:00Z",
+    titulo: 'Nota 2',
+    contenido: 'Esta es la segunda nota.',
+    fecha_creacion: '2023-09-12T12:30:00Z',
     important: true
   },
   {
     id: 3,
-    titulo: "Nota 3",
-    contenido: "Esta es la tercera nota.",
-    fecha_creacion: "2023-09-12T15:15:00Z",
+    titulo: 'Nota 3',
+    contenido: 'Esta es la tercera nota.',
+    fecha_creacion: '2023-09-12T15:15:00Z',
     important: true
-  },
-];
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
-});
-
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
   }
-});
+]
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id != id);
-  response.status(204).end();
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
+})
 
-app.post("/api/notes", (request, response) => {
-  const note = request.body;
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find((note) => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
   console.log(note)
 
-  const ids = notes.map((note) => note.id);
-  const maxId = Math.max(...ids);
+  const ids = notes.map((note) => note.id)
+  const maxId = Math.max(...ids)
 
   const newNote = {
     id: maxId + 1,
     titulo: note.titulo,
     contenido: note.contenido,
     fecha_creacion: new Date().toISOString(),
-    important: typeof note.important !== "undefined" ? note.important : false,
-  };
+    important: typeof note.important !== 'undefined' ? note.important : false
+  }
 
-  notes = [...notes, newNote];
+  notes = [...notes, newNote]
 
-  response.status(201).json(newNote);
-});
+  response.status(201).json(newNote)
+})
 
-app.put("/api/notes/:id", (request, response) => {
-  idExist = notes.some(note => note.id === Number(request.params.id));
+app.put('/api/notes/:id', (request, response) => {
+  const idExist = notes.some(note => note.id === Number(request.params.id))
   console.log(idExist)
-  
+
   if (idExist) {
-    const bodyNote = request.body;
+    const bodyNote = request.body
     console.log(bodyNote)
 
-    const oldNote = notes.find(note => note.id = request.params.id);
+    const oldNote = notes.find(note => note.id = request.params.id)
     console.log(oldNote)
 
     const newNote = {
@@ -88,25 +88,24 @@ app.put("/api/notes/:id", (request, response) => {
       contenido: oldNote.contenido !== bodyNote.contenido ? bodyNote.contenido : oldNote.contenido,
       fecha_creacion: new Date().toISOString(),
       important:
-        typeof bodyNote.important !== "undefined" &&
+        typeof bodyNote.important !== 'undefined' &&
         bodyNote.important === true
           ? oldNote.important !== bodyNote.important ? bodyNote.important : oldNote.important
           : false
     }
     console.log(newNote)
 
-    notes = notes.filter((note) => note.id !== newNote.id);
+    notes = notes.filter((note) => note.id !== newNote.id)
 
-    notes = [...notes, newNote];
+    notes = [...notes, newNote]
 
     response.status(204).json(newNote)
-  }
-  else{
+  } else {
     response.status(404).end()
   }
-});
+})
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server is running on pot ${PORT}`);
-});
+  console.log(`Server is running on pot ${PORT}`)
+})
